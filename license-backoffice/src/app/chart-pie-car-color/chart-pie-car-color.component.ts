c10f242f26754a4873a6de37dad14fbb5c091591
Tanapattara Wongkhamchan <tanapattara@kku.ac.th>
2022-08-11 23:37:13 +0700

    pie chart

--- license-backoffice/src/app/chart-pie-car-color/chart-pie-car-color.component.ts
+++ license-backoffice/src/app/chart-pie-car-color/chart-pie-car-color.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartOptions } from 'chart.js';
+import { ApiService } from '../services/api.service';
+import { FilterlicenseService } from '../services/filterlicense.service';
+import { Subscription } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { License } from '../model/license';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import DatalabelsPlugin from 'chartjs-plugin-datalabels'
 
 @Component({
   selector: 'app-chart-pie-car-color',
@@ -8,22 +16,126 @@ import { ChartOptions } from 'chart.js';
 })
 export class ChartPieCarColorComponent implements OnInit {
 
-  title = 'ng2-charts-demo';
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
+  dataSource!: MatTableDataSource<any>;
+  chartdataset: string[] = [];
   // Pie
-  public pieChartOptions: ChartOptions<'pie'> = {
-    responsive: false,
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      datalabels: {
+        formatter: (value, ctx) => {
+          if (ctx.chart.data.labels) {
+            return ctx.chart.data.labels[ctx.dataIndex];
+          }
+        },
+      },
+    }
   };
-  public pieChartLabels = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
-  public pieChartDatasets = [{
-    data: [300, 500, 100]
-  }];
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
 
-  constructor() { }
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [],
+    datasets: [{
+      data: []
+    }]
+  };
+  public pieChartType: ChartType = 'pie';
+  public pieChartPlugins = [DatalabelsPlugin];
 
+  filter: string = "";
+  notifierSubscription: Subscription = this.filterService.event.subscribe(notified => {
+    this.filter = this.filterService.getFilter();
+    this.dataSource.filter = this.filter;
+    this.displayData();
+  });
+
+  constructor(private api: ApiService, private filterService: FilterlicenseService) { }
+
+  chartDictionary = new Map<string, number>();
+
+  pieChartDatasets: string[] = [];
+
+  displayData() {
+    this.chartDictionary.clear();
+    this.pieChartData.datasets[0].data = [];
+    this.pieChartData.labels = [];
+
+    if (this.filter != '') {
+      for (let row of this.dataSource.filteredData) {
+        if (this.chartDictionary.has(row.Color)) {
+          let ex = this.chartDictionary.get(row.Color)!;
+          this.chartDictionary.set(row.Color, ex + 1);
+        }
+        else {
+          this.chartDictionary.set(row.Color, 1);
+        }
+      }
+      for (let [key, value] of this.chartDictionary) {
+        this.pieChartData.labels!.push(key);
+        this.pieChartData.datasets[0].data.push(value);
+      }
+    }
+    this.chart?.update();
+  }
   ngOnInit(): void {
+    this.getAllLicense();
+  }
+  ngOnDestroy() {
+    this.notifierSubscription.unsubscribe();
+  }
+  getAllLicense() {
+    this.api.getLicenses().
+      subscribe({
+        next: (res) => {
+          //console.log(res);
+          this.dataSource = new MatTableDataSource(res);
+          this.dataSource.filter = this.filter;
+          this.dataSource.filterPredicate = function (record, filter) {
+            var map = new Map(JSON.parse(filter));
+            let isMatch = false;
+            for (let [key, value] of map) {
+
+              var isMatchFilter: boolean = false;
+
+              if (key == 'Speed') {
+                if (value as string == 'All')
+                  isMatchFilter = true;
+                let speedFilter: number = parseInt(value as string);
+                isMatchFilter = (record[key as keyof License] <= speedFilter);
+              } else if (key == 'LicNo') {
+                if (value as string == 'All')
+                  isMatchFilter = true;
+                else
+                  isMatchFilter = (record[key as keyof License].includes(value));
+              } else if (key == 'date') {
+                let date = value as string[];
+                let sDate = new Date(date[0]);
+                let eDate = new Date(date[1]);
+                let adate_key = 'aDate', bdate_key = 'bDate'
+
+                let RecValueA = new Date(record[adate_key as keyof License]);
+                let RecValueB = new Date(record[bdate_key as keyof License]);
+
+                isMatchFilter = (RecValueA >= sDate && RecValueA <= eDate) || (RecValueB >= sDate && RecValueB <= eDate);
+              } else {
+                isMatchFilter = (record[key as keyof License] == value);
+              }
+              isMatch = (value == "All") || isMatchFilter;
+              if (!isMatch) return false;
+            }
+            return isMatch;
+          }
+
+        },
+        error: (err) => {
+          console.log("Error while fetching licenses ");
+        }
+      });
   }
 
 }
